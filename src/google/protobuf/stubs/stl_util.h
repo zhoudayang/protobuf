f74e0575f0fdc8aa47b68440deaa8803abbef478
elvinzhou <elvinzhou@tencent.com>
2022-11-06 14:54:21 +0800

    fix protobuf

--- src/google/protobuf/stubs/stl_util.h
+++ src/google/protobuf/stubs/stl_util.h
@@ -50,7 +50,14 @@ namespace protobuf {
 // place in open source code.  Feel free to fill this function in with your
 // own disgusting hack if you want the perf boost.
 inline void STLStringResizeUninitialized(std::string* s, size_t new_size) {
+#if defined(__GNUC__) && __GNUC__ <= 10
+  uint64_t *ptr = *reinterpret_cast<uint64_t **>(s);
+  char *c_ptr = reinterpret_cast<char *>(ptr);
+  ptr[-3] = new_size;
+  c_ptr[new_size] = '\0';
+#else
   s->resize(new_size);
+#endif
 }
 
 // As above, but we make sure to follow amortized growth in which we always
